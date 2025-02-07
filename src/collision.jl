b6212681f33506f215a0ceb81cc64ef5495cdf85
Leos Drahotsky <drahotskyl@gmail.com>
2025-02-07 16:33:50 +0100

    Add collision checking

--- src/collision.jl
+++ src/collision.jl
@@ -1,3 +1,12 @@
 function line_collides(start, stop, pp::PlanningSpace)
-    # TODO
+    dist = norm(stop - start)
+    steps = ceil(dist / pp.collision_resolution)
+    interpolation = (start * (1 - i / steps) + (stop * i / steps) for i in 0:steps)
+    for conf in interpolation
+        if pp.collision_check(conf)
+            return True
+        end
+    end
+
+    return False
 end
